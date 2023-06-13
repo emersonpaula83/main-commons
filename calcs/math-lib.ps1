df6686ed1ee7bf8ecc618f1def6d33d203757011
emersonpaula83 <emerson.paula@totvs.com.br>
2023-06-13 19:35:51 -0300

    commit math-lib

---
+++ calcs/math-lib.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+Realiza a subtração de dois números.
+
+.DESCRIPTION
+A função Subtrair realiza a subtração de dois números e retorna o resultado da operação.
+
+.PARAMETER numero1
+O primeiro número a ser subtraído.
+
+.PARAMETER numero2
+O segundo número a ser subtraído.
+
+.EXAMPLE
+$valor1 = 10
+$valor2 = 5
+$resultado = Subtrair $valor1 $valor2
+$resultado
+# Output: 5
+
+.NOTES
+Autor: Seu Nome
+Data: Data Atual
+Versão: 1.0
+#>
+
+function Subtrair {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [int]$numero1,
+
+        [Parameter(Mandatory = $true, Position = 1)]
+        [ValidateNotNullOrEmpty()]
+        [int]$numero2
+    )
+
+    $resultado = $numero1 - $numero2
+    return $resultado
+}
