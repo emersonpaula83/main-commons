636b90874aad6559d389d8109452b75c3a05c4a5
emersonpaula83 <emerson.paula@totvs.com.br>
2023-06-13 23:10:25 -0300

    adicional de win-functions

---
+++ win/win-functions.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+Lista todas as variáveis de ambiente da máquina.
+
+.DESCRIPTION
+Este script PowerShell lista todas as variáveis de ambiente configuradas na máquina atual. Ele exibe o nome e o valor de cada variável de ambiente na saída.
+
+.EXAMPLE
+.\ListarVariaveisAmbiente.ps1
+# Output: Nome = Valor
+
+.NOTES
+Autor: Seu Nome
+Data: Data Atual
+Versão: 1.0
+#>
+Get-ChildItem Env: | ForEach-Object {
+    $variavel = $_.Name
+    $valor = $_.Value
+    Write-Host "$variavel = $valor"
+}
